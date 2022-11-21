d96dad4f690a85ad4f57499c4ab83f00d6b8a004
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-11-21 14:45:12 +0200

    add: AppsCard mobile design href

--- components/AppsCard.tsx
+++ components/AppsCard.tsx
@@ -76,6 +76,7 @@ const AppsCard:FC<IProps> = ({img, title, href}) => {
                         Workflow Premium
                     </Text>
                     <Link
+                        href={href}
                         border='2px solid black'
                         paddingLeft='1%'
                         paddingRight='1%'
